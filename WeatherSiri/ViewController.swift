6f89e3ecd80205da70327968cbab81be2fd5baf5
Artem <Serebryakov>
2022-08-21 14:05:36 +0300

    ex02 - done

--- WeatherSiri/ViewController.swift
+++ WeatherSiri/ViewController.swift
@@ -6,19 +6,123 @@
 //
 
 import UIKit
+import ForecastIO
 import RecastAI
-import JSQMessagesViewController
+import CoreLocation
 
 class ViewController: UIViewController
 {
-    //Vars
-    var bot : RecastAIClient?
+    var recastAIBot: RecastAIClient?
+    var darkSkyBot: DarkSkyClient?
+    
+    private lazy var inputTextField : UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "City"
+        textField.textAlignment = .center
+        textField.autocorrectionType = .no
+        textField.keyboardType = .default
+        textField.returnKeyType = .done
+        textField.font = .systemFont(ofSize: UIScreen.main.bounds.height / 20)
+        return textField
+    }()
+    
+    private lazy var requestButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Request", for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(makeRequest), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var responseLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .systemBlue
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: UIScreen.main.bounds.height / 15)
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.bot = RecastAIClient(token : "YOUR_TOKEN")
-        self.bot = RecastAIClient(token : "YOUR_TOKEN", language: "YOUR_LANGUAGE")
+        view.backgroundColor = .systemBackground
+        
+        stackView.addArrangedSubview(inputTextField)
+        stackView.addArrangedSubview(requestButton)
+        stackView.addArrangedSubview(responseLabel)
+        view.addSubview(stackView)
+
+        setConstrains()
+        
+        self.recastAIBot = RecastAIClient(token: RECASTAI_TOKEN, language: "en")
+        self.darkSkyBot = DarkSkyClient(apiKey: DARK_SKY_TOKEN)
+        self.darkSkyBot?.language = .english
+    }
+    
+    func setSuccessResponse(_ response: Response) {
+        if let location = response.get(entity: "location") {
+            if let lat = location["lat"] as? CLLocationDegrees, let lng = location["lng"] as? CLLocationDegrees {
+                //print for test
+                print("Search result for: \(location["formatted"] as! String )")
+                
+                let myLoc = CLLocationCoordinate2D(latitude: lat, longitude: lng)
+                self.darkSkyBot?.getForecast(location: myLoc, completion: { (result) in
+                    switch result {
+                    case .success((let currentForcast, _)):
+                        DispatchQueue.main.async {
+                            guard var temperature = currentForcast.currently?.temperature else { return }
+                            temperature = (temperature - 32) * 5/9
+                            if let temperature = Int(temperature) as? Int {
+                                self.responseLabel.text = String(temperature) + "°C"
+                            }
+                        }
+                    case .failure(_):
+                        DispatchQueue.main.async {
+                            self.responseLabel.text = "Error"
+                        }
+                    }
+                })
+                //print for test
+                print("lat = \(lat)")
+                print("lng = \(lng)")
+            }
+        }
+        else {
+            self.responseLabel.text = "Error"
+        }
+    }
+    
+    func setFailureResponse(_ error: Error) {
+        self.responseLabel.text = "Error"
+        //print for test
+        print("error = \(error.localizedDescription)")
+    }
+    
+    @objc func makeRequest(_ sender: UIButton) {
+        if let text = self.inputTextField.text, !text.isEmpty {
+            self.responseLabel.text = "waiting..."
+            self.recastAIBot?.textRequest(text, successHandler: setSuccessResponse, failureHandle: setFailureResponse)
+        }
+    }
+    
+    func setConstrains() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            stackView.bottomAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: 2),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
     }
 }
